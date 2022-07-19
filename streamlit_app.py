460f433a3176204d8918064161a7b0fc065e6f8d
Brian Weissler <weissleb@gmail.com>
2022-07-19 00:12:59 -0400

    dunno

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,8 @@ try:
 except URLError as e:
     streamlit.error()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 def get_fruit_load_list():
-  with my_cur as my_cnx.cursor():
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
     return = my_cur.fetchall()
   
