e8edec8ed6928436973ce7f4a58a2a454f420b47
Brian Weissler <weissleb@gmail.com>
2022-07-19 00:07:59 -0400

    this fucking sucks

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,12 +41,16 @@ try:
 except URLError as e:
     streamlit.error()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+def get_fruit_load_list():
+  with my_cur as my_cnx.cursor():
+    my_cur.execute("SELECT * from fruit_load_list")
+    return = my_cur.fetchall()
+  
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+if streamlit.button('Get fruit load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
