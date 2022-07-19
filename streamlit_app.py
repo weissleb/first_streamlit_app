7df07b5e1e8a674fcc6f1d11bc60915c39d40ef6
Brian Weissler <weissleb@gmail.com>
2022-07-19 00:14:48 -0400

    dumb

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * from fruit_load_list")
-    return = my_cur.fetchall()
+    return my_cur.fetchall()
   
 streamlit.header("The fruit load list contains:")
 if streamlit.button('Get fruit load list'):
