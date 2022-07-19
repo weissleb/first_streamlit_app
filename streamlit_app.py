39e0281d029367af231274556cfcbbfc5b0d2f9e
Brian Weissler <weissleb@gmail.com>
2022-07-18 22:49:35 -0400

    advice as table

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,9 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 streamlit.header("Fruityvice Fruit Advice!")
+# write response into dataframe 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write it to the screen
+streamlit.dataframe(fruityvice_normalized)
